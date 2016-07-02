b0d39e19b409ab895f04a4afe6c6f3d44d79ea6c
Your Name Comes Here <kanyamahanga.hubert@aims-senegal.org>
2016-07-02 12:00:26 +0000

    Initial commit

---
+++ app/src/main/java/com/wutiko/hubert/myfirstapp/MyActivity.java
@@ -0,0 +1,37 @@
+package com.wutiko.hubert.myfirstapp;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.hubert.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    private void setUpActionBar() {
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
